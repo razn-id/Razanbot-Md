8d798719704fe64909733d896f231563612e47c1
razn-id <akuntest12346789@gmail.com>
2022-12-07 04:54:24 +0700

    fix Error

--- RazanBot.js
+++ RazanBot.js
@@ -2873,7 +2873,7 @@ ${vote[from][2].map((v, i) => `├ ${i + 1}. @${v.split`@`[0]}`).join("\n")}
           //     m
           //   );
           let anu = await fetchJson(
-            `https://api.razan.my.id/api/download/ytmp3?url=${url}&apikey=APIKEY`
+            `https://api.razan.my.id/api/download/ytmp3?url=${text}&apikey=APIKEY`
           );
           razan.sendMessage(
             from,
