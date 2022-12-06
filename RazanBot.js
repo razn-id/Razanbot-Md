99b2046dcccf6af0799904692e33edce57df73ec
razn-id <akuntest12346789@gmail.com>
2022-12-06 19:24:39 +0700

    Update Razanbot.js

--- RazanBot.js
+++ RazanBot.js
@@ -2895,29 +2895,40 @@ break*/
       case "ytvideo":
         {
           addCountCmd(`#${command.slice(1)}`, sender, _cmd);
-          let { ytv } = require("./lib/y2mate");
-          if (!text)
-            throw `Example : ${
-              prefix + command
-            } https://youtube.com/watch?v=PtFMh6Tccag%27 360p`;
-          let quality = args[1] ? args[1] : "360p";
-          let media = await ytv(text, quality);
-          if (media.filesize >= 100000)
-            return m.reply("File Melebihi Batas " + util.format(media));
-          razan.sendMessage(
-            from,
-            {
-              video: { url: media.dl_link },
-              mimetype: "video/mp4",
-              fileName: `${media.title}.mp4`,
-              caption: `⌕ Title : ${media.title}\n⌕ File Size : ${
-                media.filesizeF
-              }\n⌕ Url : ${isUrl(text)}\n⌕ Ext : MP3\n⌕ Resolusi : ${
-                args[1] || "360p"
-              }`,
-            },
-            { quoted: m }
+          //   let { ytv } = require("./lib/y2mate");
+          //   if (!text)
+          //     throw `Example : ${
+          //       prefix + command
+          //     } https://youtube.com/watch?v=PtFMh6Tccag%27 360p`;
+          //   let quality = args[1] ? args[1] : "360p";
+          //   let media = await ytv(text, quality);
+          //   if (media.filesize >= 100000)
+          //     return m.reply("File Melebihi Batas " + util.format(media));
+          //   razan.sendMessage(
+          //     from,
+          //     {
+          //       video: { url: media.dl_link },
+          //       mimetype: "video/mp4",
+          //       fileName: `${media.title}.mp4`,
+          //       caption: `⌕ Title : ${media.title}\n⌕ File Size : ${
+          //         media.filesizeF
+          //       }\n⌕ Url : ${isUrl(text)}\n⌕ Ext : MP3\n⌕ Resolusi : ${
+          //         args[1] || "360p"
+          //       }`,
+          //     },
+          //     { quoted: m }
+          //   );
+          // }
+
+          let anu = fetchJson(
+            `https://danzzapi.xyz/api/downloader/ytmp4?url=${text}&apikey=danzz`
           );
+
+          razan.sendMessage(from, {
+            video: { url: anu.result.url },
+            mimetype: "video/mp4",
+            fileName: `${anu.result.title}.mp4`,
+          });
         }
         break;
       case "getmusic":
